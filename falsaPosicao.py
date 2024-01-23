0425576b7f10ee88df885f18dfefb436700973e3
FolkBlues7 <viniciusgabrielprofissional42@gmail.com>
2024-01-22 21:11:20 -0300

    removendo e renomeando

--- falsaPosicao.py
+++ falsaPosicao.py
@@ -1,20 +1,24 @@
+#defina a função aqui
 def f(x):
     return x**3 - 9*x + 3
 
+#método que simular o módulo |x|
 def neverNegative(i):
     if i < 0:
         return i*(-1)
     else:
         return i
 
+#variáveis iniciais
 a = 0
 b = 1
-
-x = (a*f(b) - b*f(a))/(f(b) - f(a))
-
 precision1 = 0.005
 precision2 = 0.005
 
+#média ponderada
+x = (a*f(b) - b*f(a))/(f(b) - f(a))
+
+#iterações
 if b-a < precision1:
     x = (b+a)/2
 else:
@@ -32,15 +36,10 @@ else:
                 if f(a) * f(x) < 0:
                     b = x
                 else:
-                    a = x #44444444444
+                    a = x
                 if b - a < precision1:
                     x = (b+a)/2
                     break
                 cont = cont + 1
-                
-                
-
 
 print(str(x))
-
-
